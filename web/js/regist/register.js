220ab52d82faee75e7926d3910c628e37de86659
LandingLX <Oswardlx@163.com>
2018-07-29 12:41:10 +0800

    qqqqqqqqss

--- web/js/regist/register.js
+++ web/js/regist/register.js
@@ -1,3 +1,13 @@
-// $(function () {
-// //     $("register                                ")
-// // })
+
+function check_uname() {
+    var xhr = getXhr();
+    var uri = 'check_name/check_uname.ht?username='+$('#inputEmail');
+    xhr.open('get',encodeURI(uri),true)
+    xhr.onreadystatechange=function () {
+        if(xhr.readyState=4&&xhr.status==200){
+            var txt = xhr.responseText;
+            $('usrname_msg').innerHTML=txt;
+        }
+    };
+    xhr.send(null);
+}
